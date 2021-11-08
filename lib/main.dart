a24ccfb51f39854b6d86c4346a8a98213c7c59d6
Grzegorz <s210947@sggw.edu.pl>
2021-11-08 20:13:59 +0100

    Create button

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyButton extends StatefulWidget {
+  const MyButton({Key? key, required this.znak}) : super(key: key);
+  final String znak;
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.red,
+      child: Container(
+          width: MediaQuery.of(context).size.width / 5,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.grey, Colors.blue]),
+          ),
+          child: Text(
+            widget.znak,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width / 8,
+                fontWeight: FontWeight.w800),
+          )),
+      onTap: () {},
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -30,25 +60,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String tekst="";
+  String tekst = "";
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(child: Text(tekst)),
             SizedBox(
-              height: MediaQuery.of(context).size.width,
-              width: MediaQuery.of(context).size.width,
-            )
+                height: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    MyButton(znak: "1"),
+                    MyButton(znak: "2"),
+                    MyButton(znak: "3"),
+                    MyButton(znak: "-"),
+                    MyButton(znak: "+"),
+                  ],
+                ))
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
