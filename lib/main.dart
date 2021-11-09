7546c2a3e048838e7d8cc0bf4ea66c158ad89c59
Grzegorz <s210947@sggw.edu.pl>
2021-11-09 01:23:23 +0100

    Revert "Merge branch 'Grzegorz' into main"
    This reverts commit e8a6a6073a9bedfe6adb7274cf57e86c8e256004.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
 void main() => runApp(ChangeNotifierProvider.value(
 <<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
       value: Tekst(),
@@ -10,24 +10,33 @@ void main() => runApp(ChangeNotifierProvider.value(
 =======
 <<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
 <<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
 =======
 =======
 >>>>>>> chyba dobrze
+=======
+>>>>>>> Revert "Merge branch 'Grzegorz' into main"
 =======
   value: Tekst(),
   child: const MyApp(),
 ));
 >>>>>>> chyba dobrze
 <<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
+=======
+>>>>>>> Revert "Merge branch 'Grzegorz' into main"
 =======
 void main() {
   runApp(const MyApp());
 }
 >>>>>>> Revert "Merge branch 'Grzegorz' into main"
 >>>>>>> Revert "Merge branch 'Grzegorz' into main"
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
 =======
 >>>>>>> chyba dobrze
 >>>>>>> chyba dobrze
+=======
+>>>>>>> Revert "Merge branch 'Grzegorz' into main"
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -36,10 +45,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kalkulator',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
       home: const MyHomePage(title: 'Kalkulator'),
     );
   }
@@ -158,23 +177,24 @@ class _MyButtonState extends State<MyButton> {
           }
         }
       },
+=======
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+>>>>>>> Revert "Merge branch 'Grzegorz' into main"
     );
-  }
-}
-
-class Tekst extends ChangeNotifier {
-  String _tekst = "";
-
-  String get tekst => _tekst;
-
-  set tekst(String value) {
-    _tekst = value;
-    notifyListeners();
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -183,93 +203,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String calculateTotalValue() {
-    var state = Provider.of<Tekst>(context, listen: true);
-    String tekst = state.tekst;
-    List<double> liczby = [];
-    String znaki = "";
-    while (tekst != "") {
-      List<int> z = [
-        tekst.indexOf("+"),
-        tekst.indexOf("-"),
-        tekst.indexOf("×"),
-        tekst.indexOf("÷")
-      ];
-      z.sort();
-      int y = -1;
-      for (int i = 0; i < 4; i++) {
-        if (z[i] != -1) {
-          y = z[i];
-          break;
-        }
-      }
-      if (y == -1) y = tekst.length;
-      liczby.add(double.parse(tekst.substring(0, y)));
-      if (y != tekst.length) {
-        znaki += tekst.substring(y, y + 1);
-        y++;
-      }
-      tekst = tekst.substring(y);
-    }
-    tekst = state.tekst;
-    tekst += " = ";
-    if (liczby.length > znaki.length) {
-      while (liczby.length != 1) {
-        int x = 0;
-        if (znaki.contains("×") || znaki.contains("÷")) {
-          x = znaki.indexOf("×");
-          if (x == -1 || (znaki.contains("÷") && znaki.indexOf("÷") < x)) {
-            x = znaki.indexOf("÷");
-          }
-          if (znaki[x] == "×") {
-            liczby[x] = liczby[x] * liczby[x + 1];
-          } else {
-            liczby[x] = liczby[x] / liczby[x + 1];
-          }
-        } else {
-          if (znaki[0] == "+") {
-            liczby[x] = liczby[x] + liczby[x + 1];
-          } else {
-            liczby[x] -= liczby[x + 1];
-          }
-        }
-        znaki = znaki.substring(0, x) + znaki.substring(x + 1);
-        liczby.removeAt(x + 1);
-      }
-    }
-    tekst += liczby[0].toString();
-    return tekst;
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var state = Provider.of<Tekst>(context, listen: true);
-    String tekst = "";
-    if (state.tekst != "" && state.tekst != "błąd") {
-      tekst = calculateTotalValue();
-    }
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            widget.title,
-            style: TextStyle(
-                fontSize: 31,
-                foreground: Paint()
-                  ..shader = const LinearGradient(colors: [
-                    Colors.yellowAccent,
-                    Colors.redAccent,
-                    Colors.purpleAccent
-                  ]).createShader(
-                      const Rect.fromLTWH(60.0, 0.0, 400.0, 300.0))),
-            textAlign: TextAlign.center,
-          ),
-        ),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
       body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+<<<<<<< e8a6a6073a9bedfe6adb7274cf57e86c8e256004
             Expanded(
               child: InteractiveViewer(
                 scaleEnabled: true,
@@ -290,14 +271,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     )),
 >>>>>>> chyba dobrze
               ),
+=======
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            const MyRow(lista: ["1", "2", "3", "+"]),
-            const MyRow(lista: ["4", "5", "6", "-"]),
-            const MyRow(lista: ["7", "8", "9", "×"]),
-            const MyRow(lista: ["c", "0", "d", "÷"]),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+>>>>>>> Revert "Merge branch 'Grzegorz' into main"
+            ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
