a6afa11e119c6440db9674255af2ceec9c8920f9
Grzegorz <s210947@sggw.edu.pl>
2021-11-15 00:22:32 +0100

    Nowy przycisk dodaj linie, działająca implementacja liczb ujemnych

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(ChangeNotifierProvider.value(
-  value: Tekst(),
-  child: const MyApp(),
-));
+      value: Tekst(),
+      child: const MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -73,77 +73,116 @@ class _MyButtonState extends State<MyButton> {
                 ]),
             border: Border.all(width: 1, color: Colors.black)),
         child: Center(
-          child: widget.znak == "c"
+          child: widget.znak == "a"
               ? Icon(
-            Icons.delete_forever,
-            size: MediaQuery.of(context).size.width / 10,
-          )
-              : widget.znak == "d"
-              ? Icon(
-            Icons.backspace,
-            size: MediaQuery.of(context).size.width / 12,
-          )
-              : Text(
-            widget.znak,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: MediaQuery.of(context).size.width / 8,
-                fontWeight: FontWeight.w800),
-          ),
+                  Icons.add_to_photos,
+                  size: MediaQuery.of(context).size.width / 12,
+                )
+              : widget.znak == "c"
+                  ? Icon(
+                      Icons.delete_forever,
+                      size: MediaQuery.of(context).size.width / 10,
+                    )
+                  : widget.znak == "d"
+                      ? Icon(
+                          Icons.backspace,
+                          size: MediaQuery.of(context).size.width / 12,
+                        )
+                      : Text(
+                          widget.znak,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 8,
+                              fontWeight: FontWeight.w800),
+                        ),
         ),
       ),
       onTap: () {
         var state = Provider.of<Tekst>(context, listen: false);
-        if (widget.znak == "d") {
-          if (state.tekst != "") {
-            state.tekst = state.tekst.substring(0, state.tekst.length - 1);
+        if (widget.znak == "a") {
+          state.tekst.add("");
+          state.index++;
+        } else if (widget.znak == "d") {
+          if (state.tekst2 != "") {
+            state.tekst2 = state.tekst2.substring(0, state.tekst2.length - 1);
           }
         } else {
           if (widget.znak == "c") {
-            state.tekst = "";
+            if (state.index == 0) {
+              state.tekst2 = "";
+            } else {
+              state.tekst.removeAt(state.index);
+              state.index--;
+            }
           } else {
             bool c = false;
             for (int i = 0; i < 6; i++) {
-              if (widget.znak == ["×", "÷", "-", "+", ".", ")",][i]) c = true;
+              if (widget.znak ==
+                  [
+                    "×",
+                    "÷",
+                    "-",
+                    "+",
+                    ".",
+                    ")",
+                  ][i]) c = true;
             }
             if (c) {
               c = false;
               for (int i = 0; i < 5; i++) {
-                if (state.tekst.isEmpty ||
-                    state.tekst[state.tekst.length - 1] ==
+                if (state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] ==
                         ["×", "÷", "-", "+", "."][i]) c = true;
               }
-    if(widget.znak=="."){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == "("))c=true;}
-    if(widget.znak=="."){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == ")"))c=true;}
-    if(widget.znak=="÷"){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == "("))c=true;}
-    if(widget.znak=="×"){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == "("))c=true;}
-    if(widget.znak=="+"){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == "("))c=true;}
-    if(widget.znak==")"){if((state.tekst.isEmpty || state.tekst[state.tekst.length - 1] == "("))c=true;
-              int ile = 0;
-              int value = state.tekst.length;
-              for (int i = 0; i < value; i++) {
-                if (state.tekst[i] == "(") ile++;
-                if (state.tekst[i] == ")") ile--;}
-              if(ile<1)c=true;
+              if (widget.znak == ".") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == "(")) c = true;
+              }
+              if (widget.znak == ".") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == ")")) c = true;
+              }
+              if (widget.znak == "÷") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == "(")) c = true;
+              }
+              if (widget.znak == "×") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == "(")) c = true;
+              }
+              if (widget.znak == "+") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == "(")) c = true;
+              }
+              if (widget.znak == ")") {
+                if ((state.tekst2.isEmpty ||
+                    state.tekst2[state.tekst2.length - 1] == "(")) c = true;
+                int ile = 0;
+                int value = state.tekst2.length;
+                for (int i = 0; i < value; i++) {
+                  if (state.tekst2[i] == "(") ile++;
+                  if (state.tekst2[i] == ")") ile--;
+                }
+                if (ile < 1) c = true;
               }
             }
             if (widget.znak == ".") {
-              for (int i = state.tekst.length - 1; i > 0; i--) {
+              for (int i = state.tekst2.length - 1; i > 0; i--) {
                 bool czy = false;
                 for (int j = 0; j < 4; j++) {
-                  if (state.tekst[i] == ["×", "÷", "-", "+"][j]) {
+                  if (state.tekst2[i] == ["×", "÷", "-", "+"][j]) {
                     czy = true;
                     break;
                   }
                 }
                 if (czy) break;
-                if (state.tekst[i] == ".") {
+                if (state.tekst2[i] == ".") {
                   c = true;
                   break;
                 }
               }
             }
-            if (!c) state.tekst += widget.znak;
+            if (!c) state.tekst2 += widget.znak;
           }
         }
       },
@@ -152,12 +191,27 @@ class _MyButtonState extends State<MyButton> {
 }
 
 class Tekst extends ChangeNotifier {
-  String _tekst = "";
+  List<String> _tekst = [""];
+  int _index = 0;
 
-  String get tekst => _tekst;
+  int get index => _index;
 
-  set tekst(String value) {
+  set index(int value) {
+    _index = value;
+    notifyListeners();
+  }
+
+  List<String> get tekst => _tekst;
+
+  set tekst(List<String> value) {
     _tekst = value;
+    notifyListeners();
+  }
+
+  String get tekst2 => _tekst[index];
+
+  set tekst2(String value) {
+    _tekst[index] = value;
     notifyListeners();
   }
 }
@@ -172,36 +226,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  String usunNawiasy(String tekst){
-    int ile = 0,start=-1,end=-1;
-    for (int i = 0; i < tekst.length; i++) {
-      if (tekst[i] == "(") {ile++;if(i+i<tekst.length&&tekst[i+1]=="-"){
-        tekst=tekst.substring(0,i)+"0"+tekst.substring(i);
-      }}
-      if (tekst[i] == ")") ile--;
-      if(ile!=0&&start==-1)start=i;
-      if(ile==0&&start!=-1){end=i;
-      String z1="",z2="",w;
-      if(start!=0&&["0", "1", "2", "3","4","5","6","7","8","9"].any((element) {if(element != tekst[start-1])return false;return true;}))z1="×";
-      if(tekst.length>end+1&&["×", "÷", "-", "+"].every((element) {if(element!=tekst[end+1])return true;return false;}))z2="×";
-      w=calculateTotalValue(tekst.substring(start+1,end));
-      tekst = tekst.substring(0,start)+z1+w+z2+tekst.substring(end+1);
-      start=-1;
-      end=-1;
-      tekst = usunNawiasy(tekst);
-      break;
-      }
+  String zamienMinusy(String tekst) {
+    if (tekst.contains("(-")) {
+      tekst = tekst.substring(0, tekst.indexOf("(-") + 1) +
+          "u" +
+          tekst.substring(tekst.indexOf("(-") + 2);
+      zamienMinusy(tekst);
     }
-    if(start!=-1)return "?";
     return tekst;
   }
+
+  String usunNawiasy(String tekst) {
+    int ile = 0, start = -1, end = -1;
+    for (int i = 0; i < tekst.length; i++) {
+      if (tekst[i] == "(") {
+        ile++;
+      }
+      if (tekst[i] == ")") ile--;
+      if (ile != 0 && start == -1) start = i;
+      if (ile == 0 && start != -1) {
+        end = i;
+        String z1 = "", z2 = "", w;
+        if (start != 0 &&
+            ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].any((element) {
+              if (element != tekst[start - 1]) return false;
+              return true;
+            })) z1 = "×";
+        if (tekst.length > end + 1 &&
+            ["×", "÷", "-", "+"].every((element) {
+              if (element != tekst[end + 1]) return true;
+              return false;
+            })) z2 = "×";
+        w = calculateTotalValue(tekst.substring(start + 1, end));
+        if (w[0] == "-") w = "u" + w.substring(1);
+        tekst =
+            tekst.substring(0, start) + z1 + w + z2 + tekst.substring(end + 1);
+        start = -1;
+        end = -1;
+        tekst = usunNawiasy(tekst);
+        if (tekst[0] == "-") tekst = "u" + tekst.substring(1);
+        break;
+      }
+    }
+    if (start != -1) return "?";
+    return tekst;
+  }
+
   String calculateTotalValue(String tekstBazowy) {
     String tekst = tekstBazowy;
-    if(["×", "÷", "-", "+", "."].any((element){ if(tekst[tekst.length-1]==element)return true;return false;})){return "?";}
-    if(tekst=="?"||tekst =="(")return "?";
+    tekst = zamienMinusy(tekst);
+    if (["×", "÷", "-", "+", "."].any((element) {
+      if (tekst[tekst.length - 1] == element) return true;
+      return false;
+    })) {
+      return "?";
+    }
+    if (tekst == "?" || tekst == "(") return "?";
     tekst = usunNawiasy(tekst);
-    if(tekst=="?"||tekst =="(")return "?";
+    if (tekst == "?" || tekst == "(") return "?";
     List<double> liczby = [];
     String znaki = "";
     while (tekst != "") {
@@ -220,8 +302,9 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
       if (y == -1) y = tekst.length;
-      if (tekst[y - 1] == ".") {
-        liczby.add(double.parse(tekst.substring(0, y - 1)));
+      if (y == 0) y = tekst.length;
+      if (tekst[0] == "u" || tekst[0] == "-") {
+        liczby.add(-double.parse(tekst.substring(1, y)));
       } else {
         liczby.add(double.parse(tekst.substring(0, y)));
       }
@@ -261,16 +344,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var state = Provider.of<Tekst>(context, listen: true);
-    String tekst = "";
-    if (state.tekst != "") {
-      tekst = state.tekst + " = " + calculateTotalValue(state.tekst);
-    }
     return Scaffold(
       appBar: AppBar(
+        elevation: 8,
+        shadowColor: Colors.lightBlueAccent,
         title: Center(
           child: Text(
             widget.title,
             style: TextStyle(
+              shadows: [Shadow(
+                color: Colors.black,
+                blurRadius: 2,
+                offset: Offset(1,1)
+              )],
                 fontSize: 31,
                 foreground: Paint()
                   ..shader = const LinearGradient(colors: [
@@ -289,21 +375,58 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Expanded(
               child: InteractiveViewer(
-                scaleEnabled: true,
-                maxScale: 3,
-                child: Center(
-                    child: Text(
-                      tekst,
-                      style: const TextStyle(
-                        fontSize: 20,
-                      ),
-                    )),
-              ),
+                  scaleEnabled: true,
+                  maxScale: 2,
+                  child: ListView.builder(
+                      itemCount: state.index + 1,
+                      itemBuilder: (context, index) {
+                        String tekst = "";
+                        if (state.tekst[index] != "") {
+                          tekst = state.tekst[index] +
+                              " = " +
+                              calculateTotalValue(state.tekst[index]);
+                        }
+                        return Container(
+                          padding: const EdgeInsets.all(13),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            gradient: LinearGradient(
+                                begin: Alignment.bottomRight,
+                                end: Alignment.topLeft,
+                                colors: [
+                                  Color(0xffc0c0c0),
+                                  Color(0xffd0d0d0),
+                                  Color(0xffe0e0e0),
+                                  Color(0xfff0f0f0)
+                                ],
+                                stops: [
+                                  0,0.2,0.4,1
+                                ]),
+                            border: Border(
+                                bottom:
+                                    BorderSide(color: Colors.black, width: 2)),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black87,
+                                  blurRadius: 7,
+                                  spreadRadius: 2,
+                                  offset: Offset(0, 0))
+                            ],
+                          ),
+                          child: Center(
+                              child: Text(
+                            tekst,
+                            style: const TextStyle(
+                              fontSize: 20,
+                            ),
+                          )),
+                        );
+                      })),
             ),
             const MyRow(lista: ["1", "2", "3", "(", ")"]),
             const MyRow(lista: ["4", "5", "6", "+", "-"]),
             const MyRow(lista: ["7", "8", "9", "×", "÷"]),
-            const MyRow(lista: ["c", "0", "d", ".", ""]),
+            const MyRow(lista: ["c", "0", "d", ".", "a"]),
           ],
         ),
       ),
